49f3cfad4b1b3b341becca01e5211a506c6e2454
Riki aldi pari <rikialdipari@gmail.com>
2019-08-22 13:22:33 +0700

    Create PasswordConstraintValidator.java

---
+++ validasi-password/PasswordConstraintValidator.java
@@ -0,0 +1,40 @@
+package com.management_user.api.test;
+
+
+import com.google.common.base.Joiner;
+import org.passay.*;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public void initialize(ValidPassword arg0) {
+    }
+    //contoh A1#324rthklo  jika diaktifina smua
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+                new LengthRule(8, 30),
+                new UppercaseCharacterRule(1),
+                new DigitCharacterRule(1),
+//                new SpecialCharacterRule(1),
+                new NumericalSequenceRule(8,false),
+                new AlphabeticalSequenceRule(8,false),
+                new QwertySequenceRule(8,false),
+                new WhitespaceRule()));
+
+        RuleResult result = validator.validate(new PasswordData(password));
+        if (result.isValid()) {
+            return true;
+        }
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(
+                Joiner.on(",").join(validator.getMessages(result)))
+                .addConstraintViolation();
+        return false;
+    }
+}
