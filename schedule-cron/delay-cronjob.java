005c45c7363f2beeff98c6009e9298d133139c96
Riki aldi pari <rikialdipari@gmail.com>
2019-09-04 10:49:31 +0700

    Create delay-cronjob.java

---
+++ schedule-cron/delay-cronjob.java
@@ -0,0 +1,46 @@
+1. main : @EnableScheduling // khusus delay 
+
+2. java
+package com.bawahtangga.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @Scheduled(fixedRate = 2000)
+    public void scheduleTaskWithFixedRate() {
+        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void scheduleTaskWithFixedDelay() {
+        logger.info("Fixed Delay Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            logger.error("Ran into an error {}", ex);
+            throw new IllegalStateException(ex);
+        }
+    }
+    @Scheduled(fixedRate = 2000, initialDelay = 5000)
+    public void scheduleTaskWithInitialDelay() {
+        logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+
+    }
+
+    @Scheduled(cron = "0 * 9 * * ?")
+    public void scheduleTaskWithCronExpression() {
+
+        System.out.println("Java cron job expression:: " +dateTimeFormatter.format(LocalDateTime.now()));
+    }
+}
